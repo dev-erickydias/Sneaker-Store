1e1ecdbccfca164d3767d7e024b06463fc0f7dfc
Ericky Dias <erickyhenriquesd@gmail.com>
2024-02-15 17:56:17 +0000

    add component Header & your style

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+  <header className="header">
+    <p className="headerParagraph">Frete grátis para todo o Brasil</p>
+  </header>
+  )
+}
+
+export default Header;
