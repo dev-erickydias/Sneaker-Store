085d84dc4bcaf37861da79d68842b7b36f0f9d78
Ericky Dias <erickyhenriquesd@gmail.com>
2024-02-15 19:04:44 +0000

    finalizando o component Header e seu estilo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,24 @@
 import "./header.css";
+import logo from "../../image/logo.png";
 
 function Header() {
   return (
-  <header className="header">
-    <p className="headerParagraph">Frete grátis para todo o Brasil</p>
-  </header>
-  )
+    <header className="header">
+      <nav className="nav">
+        <p className="navParagraph">Frete grátis para todo o Brasil</p>
+      </nav>
+      <section className="hero">
+        <img src={logo} className="heroLogo" />
+        <div className="heroTexts">
+          <h2 className="HeroTitle">A melhor loja de Jordan</h2>
+          <p className="HeroParagraph">
+            O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e
+            o jogador Michael Jordan.
+          </p>
+        </div>
+      </section>
+    </header>
+  );
 }
 
 export default Header;
